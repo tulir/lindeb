65d841e2d050e3c11febeedcae17dc4dbbb8da00
Tulir Asokan <tulir@maunium.net>
2017-12-25 21:56:06 +0200

    Remove and edit default React source files

---
+++ frontend/src/lindeb.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Lindeb extends Component {
+  render() {
+    return (
+      <div className="lindeb">
+      </div>
+    );
+  }
+}
+
+export default Lindeb;
