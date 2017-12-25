5914f7924ad8cbe5761dd9fd1d73520c3bc1a792
Tulir Asokan <tulir@maunium.net>
2017-12-25 21:56:46 +0200

    Move style/ to src/

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../style/index.sass';
+import './style/index.sass';
 import Lindeb from './lindeb';
 
 ReactDOM.render(<Lindeb />, document.getElementById('root'));
